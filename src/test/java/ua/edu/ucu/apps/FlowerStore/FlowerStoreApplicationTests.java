07d0bb6b205a94832f604a0d47844eafb6c9ecd9
Sofia Sampara <sampara.pn@ucu.edu.ua>
2024-10-26 21:33:03 +0300

    Try1 to test my package

--- src/test/java/ua/edu/ucu/apps/FlowerStore/FlowerStoreApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/FlowerStore/FlowerStoreApplicationTests.java
@@ -2,8 +2,10 @@ package ua.edu.ucu.apps.FlowerStore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlowerStoreApplicationTests {
 
 	@Test
